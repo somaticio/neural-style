0fbb88e636ee10ee4eb15e387889f22946aed5f6
Matt D <mattdavids@gmail.com>
2015-10-21 23:42:49 -0400

    add abs path to th command

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/neural-style
-th /home/ubuntu/neural-style/neural_style.lua -gpu 0 -backend cudnn
+/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 0 -backend cudnn
 
