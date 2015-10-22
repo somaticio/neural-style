37f9641d5583dd86ac80cf1f22f0a506fd41c474
Matt D <mattdavids@gmail.com>
2015-10-22 00:17:37 -0400

    update test and train scripts to work correctly with somaticagent

--- train.sh
+++ train.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. /home/ubuntu/.bashrc
+. /home/ubuntu/.profile
 cd /home/ubuntu/neural-style
-/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 0 -backend cudnn
-
+/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 0 -print_iter 1
