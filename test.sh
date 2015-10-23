48af05885643b7f67a140007c4d74982cb990e87
Jason Toy <jasontoy@gmail.com>
2015-10-23 10:33:33 +0200

    hardcode output for now

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 . /home/ubuntu/.profile
 . /home/ubuntu/.bashrc
 cd /home/ubuntu/neural-style
-#/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 0 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image /home/ubuntu/neur
-/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 0 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image $1 -output_image output.jpg
-echo output.png
+#/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 3 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image /home/ubuntu/neur
+/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 0 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image $1 -output_image /home/ubuntu/output.jpg
+echo /hoe/ubuntu/output.jpg
