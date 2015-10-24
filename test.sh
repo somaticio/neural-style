fae016224f5fc68106c7d51a6a0ec74cad215d6f
Jason Toy <jasontoy@gmail.com>
2015-10-24 22:58:45 +0200

    wrong path

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@
 cd /home/ubuntu/neural-style
 #/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 3 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image /home/ubuntu/neur
 /home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 0 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image $1 -output_image /home/ubuntu/output.jpg
-echo /hoe/ubuntu/output.jpg
+echo /home/ubuntu/output.jpg
