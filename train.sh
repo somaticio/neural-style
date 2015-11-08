3c7cb551f98087966f53c7ff583e7789bee6be26
jtoy <jtoy@jtoy.net>
2015-11-08 16:45:19 -0600

    Update train.sh
    updated paths

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 . /home/ubuntu/.bashrc
 . /home/ubuntu/.profile
-cd /home/ubuntu/neural-style
-/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 0 -print_iter 1
+cd /home/ubuntu/experiment
+/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua -gpu 0 -print_iter 1
