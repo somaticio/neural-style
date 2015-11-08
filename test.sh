313b6a522dfbd585c8398881818a0bb63f3c044b
jtoy <jtoy@jtoy.net>
2015-11-08 16:44:46 -0600

    Update test.sh
    updated paths

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . /home/ubuntu/.profile
 . /home/ubuntu/.bashrc
-cd /home/ubuntu/neural-style
+cd /home/ubuntu/experiment
 #/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 3 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image /home/ubuntu/neur
 /home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 0 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image $1 -output_image /home/ubuntu/output.jpg
 echo /home/ubuntu/output.jpg
