45598b056c4cabb6bdabb17164d553438ecbb756
Ubuntu <ubuntu@ip-172-31-5-53.ec2.internal>
2015-11-14 22:41:33 +0000

    use unix time for output for now

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@
 . /home/ubuntu/.bashrc
 cd /home/ubuntu/experiment
 #/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 3 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image /home/ubuntu/neur
-OUTPUT="$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)"
+#OUTPUT="$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32)"
+OUTPUT="$(date +%s)"
+#OUTPUT="$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)"
 /home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua -gpu 0 -print_iter 1 -num_iterations 10 -style_image /home/ubuntu/experiment/examples/inputs/escher_sphere.jpg -content_image $1 -output_image "/home/ubuntu/$OUTPUT.jpg"   > /dev/null 2>&1
 echo /home/ubuntu/$OUTPUT.jpg
