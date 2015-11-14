e95fa92c2ba56edd77377a98761ccb63402a3a8c
Jason Toy <jasontoy@gmail.com>
2015-11-14 15:59:13 -0600

    use yaml

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@
 . /home/ubuntu/.bashrc
 cd /home/ubuntu/experiment
 #/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 3 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image /home/ubuntu/neur
-/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua -gpu 0 -print_iter 1 -num_iterations 10 -style_image /home/ubuntu/experiment/examples/inputs/escher_sphere.jpg -content_image $1 -output_image /home/ubuntu/output.jpg   > /dev/null 2>&1
-echo /home/ubuntu/output.jpg
+OUTPUT="$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)"
+/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua -gpu 0 -print_iter 1 -num_iterations 10 -style_image /home/ubuntu/experiment/examples/inputs/escher_sphere.jpg -content_image $1 -output_image "/home/ubuntu/$OUTPUT.jpg"   > /dev/null 2>&1
+echo /home/ubuntu/$OUTPUT.jpg
