e4f3a75aa3bf4aa3ca0426e95d3b8ce56026d3b4
Jason Toy <jasontoy@gmail.com>
2015-11-30 16:53:29 -0600

    skip if files dont exist

--- test.sh
+++ test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-. /home/ubuntu/.profile
-. /home/ubuntu/.bashrc
+if [ -f /home/ubuntu/.profile ]; then
+ . /home/ubuntu/.profile
+else
+  echo "warning .profile doesnt exist"
+fi
+if [ -f /home/ubuntu/.bashrc ]; then
+ . /home/ubuntu/.bashrc
+else
+  echo "warning .bashrc doesnt exist"
+fi
 cd /home/ubuntu/experiment
 #/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 3 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image /home/ubuntu/neur
 OUTPUT="$(date +%s)"
