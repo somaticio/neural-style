581e961458fc5086ae74bd0ade1a691ebbbf00dc
Jason Toy <jasontoy@gmail.com>
2015-12-01 20:04:18 -0600

    almost ready for docker

---
+++ testsuite.sh
@@ -0,0 +1,2 @@
+docker run -d -p 5000:5000 somatic/neural-style  python /home/ubuntu/somaticagent/web.py -s
+curl --fail -X POST -F input_image=@input.jpg -F content_image=@content.jpg -F output_image=blah.jpg -F gpu=-1 http://127.0.0.1:5000/run
