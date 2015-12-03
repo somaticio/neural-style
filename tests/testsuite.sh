c09b9869761a2c5fd9b07895ea9b64ac668557a5
Jason Toy <jasontoy@gmail.com>
2015-12-03 17:41:18 -0600

    dont build docker in test

--- tests/testsuite.sh
+++ tests/testsuite.sh
@@ -1,3 +1,3 @@
-docker build -t somatic/neural-style .
+#docker build -t somatic/neural-style .
 docker run -d -p 5000:5000 somatic/neural-style  python /home/ubuntu/somaticagent/web.py -s
-curl --fail -X POST -F input_image=@input.jpg -F content_image=@content.jpg -F output_image=blah.jpg -F gpu=-1 http://127.0.0.1:5000/run
+curl --fail -X POST -F style_image=@tests/starry.jpg -F content_image=@tests/slawek.jpg -F output_image=blah.jpg -F gpu=-1 http://127.0.0.1:5000/run
