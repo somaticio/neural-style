00aa62d79f7db04e03dddd8fa0da00fc3e21465e
Jason <jasontoy@gmail.com>
2015-12-16 23:37:53 +0000

    dont display this warning

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ -f /home/ubuntu/.profile ]; then
  . /home/ubuntu/.profile
-else
-  echo "warning .profile doesnt exist"
+#else
+#  echo "warning .profile doesnt exist"
 fi
 if [ -f /home/ubuntu/.bashrc ]; then
  . /home/ubuntu/.bashrc
-else
-  echo "warning .bashrc doesnt exist"
+#else
+#  echo "warning .bashrc doesnt exist"
 fi
 cd /home/ubuntu/experiment
 #/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 3 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image /home/ubuntu/neur
