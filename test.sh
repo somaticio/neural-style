3884f9df7fd2fd180d0befb2013ac70ee8901b5a
Krzysztof Baranski <baranski.kk@gmail.com>
2015-12-18 16:45:01 +0100

    Fix paths problem, warnings to stderr

--- test.sh
+++ test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 if [ -f /home/ubuntu/.profile ]; then
  . /home/ubuntu/.profile
-#else
-#  echo "warning .profile doesnt exist"
+else
+  >&2 echo "warning .profile doesnt exist"
 fi
 if [ -f /home/ubuntu/.bashrc ]; then
  . /home/ubuntu/.bashrc
-#else
-#  echo "warning .bashrc doesnt exist"
+else
+  >&2 echo "warning .bashrc doesnt exist"
 fi
 cd /home/ubuntu/experiment
 #/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 3 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image /home/ubuntu/neur
-OUTPUT="$(date +%s)"
-/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua -gpu -1 -output_image /home/ubuntu/$OUTPUT.jpg $@ >> /home/ubuntu/logs 2>&1
+OUTPUT="/home/ubuntu/$(date +%s).jpg"
+/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua $@ -output_image "$OUTPUT" >> /home/ubuntu/logs 2>&1
 #/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua -gpu 0 -print_iter 1 -num_iterations 10 -style_image /home/ubuntu/experiment/examples/inputs/escher_sphere.jpg -content_image $1 -output_image "/home/ubuntu/$OUTPUT.jpg"   > /dev/null 2>&1
-echo /home/ubuntu/$OUTPUT.jpg
+echo $OUTPUT
