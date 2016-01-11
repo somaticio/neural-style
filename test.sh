df3711e242475fe65133d90e350c7ac05fe8bdb7
root <root@ip-172-31-2-179.ec2.internal>
2016-01-11 02:20:53 +0000

    docker file works with our own images

--- test.sh
+++ test.sh
@@ -10,9 +10,7 @@ else
   >&2 echo "warning .bashrc doesnt exist"
 fi
 cd /home/ubuntu/experiment
-#/home/ubuntu/torch/install/bin/th /home/ubuntu/neural-style/neural_style.lua -gpu 3 -print_iter 1 -style_image /home/ubuntu/neural-style/examples/inputs/escher_sphere.jpg -content_image /home/ubuntu/neur
 OUTPUT="/home/ubuntu/$(date +%s).jpg"
 : ${GPU:=-1}
-th /home/ubuntu/experiment/neural_style.lua  -gpu $GPU $@ -output_image "$OUTPUT" >> /home/ubuntu/logs 2>&1
-#/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua -gpu 0 -print_iter 1 -num_iterations 10 -style_image /home/ubuntu/experiment/examples/inputs/escher_sphere.jpg -content_image $1 -output_image "/home/ubuntu/$OUTPUT.jpg"   > /dev/null 2>&1
+/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua  -gpu $GPU $@ -output_image "$OUTPUT" >> /home/ubuntu/logs 2>&1
 echo $OUTPUT
