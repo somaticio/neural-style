af7ddb6b254b49b2243536057d6034538e26112e
Jason Toy <jasontoy@gmail.com>
2016-02-08 18:59:09 -0600

    cleanups

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ else
   >&2 echo "warning .bashrc doesnt exist"
 fi
 cd /home/ubuntu/experiment
-OUTPUT="/home/ubuntu/$(date +%s).jpg"
+OUTPUT="/data/output/$(date +%s).jpg"
 : ${GPU:=-1}
 /home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua  -gpu $GPU $@ -output_image "$OUTPUT" >> /home/ubuntu/logs 2>&1
 echo $OUTPUT
